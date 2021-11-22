3745bd6e4a869733a75bc88869b9c50385959913
Satyam Sutapalli <satyam@Satyams-Mac.local>
2021-11-22 11:18:16 +0530

    adding check in out list

--- Viba/Viba/ViewControllers/Dashboard/TabControllers/DashboardViewController.swift
+++ Viba/Viba/ViewControllers/Dashboard/TabControllers/DashboardViewController.swift
@@ -110,3 +110,13 @@ class DashboardViewController: UIViewController {
         }
     }
 }
+
+extension DashboardViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
