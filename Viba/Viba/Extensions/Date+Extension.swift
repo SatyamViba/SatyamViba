3470e1cfadb12401527ca16d19e3d818205d4fe9
Satyam Sutapalli <satyam@Satyams-Mac.local>
2021-11-23 20:43:16 +0530

    updating the date selection in time sheets

--- Viba/Viba/Extensions/Date+Extension.swift
+++ Viba/Viba/Extensions/Date+Extension.swift
@@ -10,9 +10,25 @@ import Foundation
 extension Date {
     func subtract(years: Int) -> Date {
         guard let result =  Calendar.current.date(byAdding: .year, value: -(years), to: self) else {
-            return Date()
+            return self
         }
         
+        return result
+    }
+
+    func add(days: Int) -> Date {
+        guard let result =  Calendar.current.date(byAdding: .day, value: days, to: self) else {
+            return self
+        }
+
+        return result
+    }
+
+    func subtract(days: Int) -> Date {
+        guard let result =  Calendar.current.date(byAdding: .day, value: -(days), to: self) else {
+            return self
+        }
+
         return result
     }
 
@@ -21,4 +37,19 @@ extension Date {
         dtFormatter.dateFormat = "dd/MM/yyyy hh:mm a"
         return dtFormatter.string(from: self)
     }
+
+    func isSameDate(_ comparisonDate: Date) -> Bool {
+        let order = Calendar.current.compare(self, to: comparisonDate, toGranularity: .day)
+        return order == .orderedSame
+      }
+
+      func isBeforeDate(_ comparisonDate: Date) -> Bool {
+        let order = Calendar.current.compare(self, to: comparisonDate, toGranularity: .day)
+        return order == .orderedAscending
+      }
+
+      func isAfterDate(_ comparisonDate: Date) -> Bool {
+        let order = Calendar.current.compare(self, to: comparisonDate, toGranularity: .day)
+        return order == .orderedDescending
+      }
 }
--- Viba/Viba/ViewControllers/Dashboard/TabControllers/TimesheetViewController.swift
+++ Viba/Viba/ViewControllers/Dashboard/TabControllers/TimesheetViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import FontAwesome_swift
+import DatePicker
 
 class TimesheetViewController: UIViewController {
-
+    private let minDate = DatePickerHelper.shared.dateFrom(day: 01, month: 01, year: 2000)!
     @IBOutlet weak var eventList: UITableView!
     @IBOutlet weak var timesheetImage: UIImageView!
 
@@ -17,20 +19,88 @@ class TimesheetViewController: UIViewController {
     @IBOutlet weak var calendar: UIButton!
     @IBOutlet weak var right: UIButton!
 
-    @IBOutlet weak var wfoImg: UIImageView!
+    @IBOutlet weak var wfoImg: VibaCircularImage!
     @IBOutlet weak var wfoTime: UILabel!
 
-    @IBOutlet weak var wfhImg: UIImageView!
+    @IBOutlet weak var wfhImg: VibaCircularImage!
     @IBOutlet weak var wfhTime: UILabel!
 
-    @IBOutlet weak var durationImg: UIImageView!
+    @IBOutlet weak var durationImg: VibaCircularImage!
     @IBOutlet weak var duration: UILabel!
+
+    var selectedDate = Date()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         eventList.register(UINib(nibName: "ClockInOutTableViewCell", bundle: nil), forCellReuseIdentifier: "ClockInOut")
+
+        let leftImg = UIImage.fontAwesomeIcon(code: FontAwesome.angleLeft.rawValue, style: .solid, textColor: .black, size: CGSize(width: 26, height: 26))
+        left.setImage(leftImg, for: .normal)
+        let leftDiabledImg = UIImage.fontAwesomeIcon(code: FontAwesome.angleLeft.rawValue, style: .solid, textColor: .lightGray, size: CGSize(width: 26, height: 26))
+        left.setImage(leftDiabledImg, for: .disabled)
+
+        let rightImg = UIImage.fontAwesomeIcon(code: FontAwesome.angleRight.rawValue, style: .solid, textColor: .black, size: CGSize(width: 26, height: 26))
+        right.setImage(rightImg, for: .normal)
+        let rightDisabledImg = UIImage.fontAwesomeIcon(code: FontAwesome.angleRight.rawValue, style: .solid, textColor: .lightGray, size: CGSize(width: 26, height: 26))
+        right.setImage(rightDisabledImg, for: .normal)
+
+        let calImg = UIImage.fontAwesomeIcon(code: FontAwesome.calendarAlt.rawValue, style: .regular, textColor: .black, size: CGSize(width: 26, height: 26))
+        calendar.setImage(calImg, for: .normal)
+
+        timesheetImage.image = UIImage.fontAwesomeIcon(name: .userClock, style: .solid, textColor: .black, size: CGSize(width: 16, height: 16))
+        wfoImg.image = UIImage.fontAwesomeIcon(name: .arrowUp, style: .solid, textColor: .black, size: CGSize(width: 16, height: 16))
+        wfhImg.image = UIImage.fontAwesomeIcon(name: .arrowUp, style: .solid, textColor: .black, size: CGSize(width: 16, height: 16))
+        durationImg.image = UIImage.fontAwesomeIcon(name: .clock, style: .solid, textColor: .black, size: CGSize(width: 16, height: 16))
+
+        formatAndShowDate()
+    }
+
+    @IBAction func showPreviousDay(_ sender: Any) {
+        selectedDate = selectedDate.subtract(days: 1)
+        formatAndShowDate()
+        updatePrevNextOptions()
+    }
+
+    @IBAction func showNextDay(_ sender: Any) {
+        selectedDate = selectedDate.add(days: 1)
+        formatAndShowDate()
+        updatePrevNextOptions()
+    }
+
+    @IBAction func selectDate(_ sender: Any) {
+        // Create picker object
+        let datePicker = DatePicker()
+        datePicker.setup(beginWith: Date(), min: minDate, max: Date()) { [self] (selected, date) in
+            if selected, let seldDate = date {
+                selectedDate = seldDate
+                formatAndShowDate()
+                updatePrevNextOptions()
+            }
+        }
+        // Display
+        datePicker.show(in: self, on: nil)
+    }
+
+    private func formatAndShowDate() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE d MMM yyyy"
+        self.selectedDateLabel.text = formatter.string(from: selectedDate)
+    }
+
+    private func updatePrevNextOptions() {
+        let today = Date()
+        if selectedDate.isSameDate(today) {
+            right.isEnabled = false
+            left.isEnabled = true
+        } else if selectedDate.isSameDate(minDate) {
+            right.isEnabled = true
+            left.isEnabled = false
+        } else if selectedDate.isBeforeDate(today) && today.isAfterDate(selectedDate) {
+            right.isEnabled = true
+            left.isEnabled = true
+        }
     }
 }
 
